14c8024744fcc4262fddce0f098b2a352de1cf0b
AkshayMent <akshay@ment.tech>
2023-07-20 12:17:03 +0530

    base url changed

--- src/services/baseCall.js
+++ src/services/baseCall.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const baseUrl = "http://localhost:5000";
+// const baseUrl = "http://localhost:5000";
+const baseUrl = "https://web3-origin.onrender.com";
 export const BASE_CALL = {
   post: async (url, payload) => await axios.post(baseUrl + url, payload),
   get: async (url) => await axios.get(baseUrl + url)
